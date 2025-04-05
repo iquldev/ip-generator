0a14484bc51559d79e3a0b5ad4b1dd71a23d681d
iquldev <141398177+iquldev@users.noreply.github.com>
2025-04-05 14:57:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import random as r
 count = int(input('Enter the number of ip addresses: '))
 
 for i in range(count):
-	ip = [r.randint(0, 255),r.randint(0,255),r.        randint(0,255),r.randint(0,255)]
+	ip = [r.randint(0, 255),r.randint(0,255),r.randint(0,255),r.randint(0,255)]
 	print(f'{ip[0]}.{ip[1]}.{ip[2]}.{ip[3]}')
